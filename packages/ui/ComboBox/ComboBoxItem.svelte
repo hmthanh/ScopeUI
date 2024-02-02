9cbfbb9758a95a13fd5027900a8737067e00d12c
ThanhHM2 <thanhhm2@vng.com.vn>
2024-02-02 17:49:32 +0700

    add ComboBox

---
+++ packages/ui/ComboBox/ComboBoxItem.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    // import { createEventForwarder } from "$lib/internal.txt";
+    // import { get_current_component } from "svelte/internal";
+
+    import {createEventDispatcher} from 'svelte';
+
+    export let selected = false;
+    export let disabled = false;
+
+    let className = '';
+    export {className as class};
+
+    const dispath = createEventDispatcher();
+</script>
+
+<!--tabindex="0"-->
+<!--	{className}-->
+<li
+        {...$$restProps}
+        class="combo-box-item {className}"
+
+        class:disabled
+        class:selected
+>
+    <slot name="icon"/>
+    <span>
+		<slot/>
+	</span>
+</li>
+
+<style lang="css">
+    @import "ComboBoxItem.css";
+</style>
