8c1da6d4cc0709c9d9b8a4e5e057d406218093bb
ThanhHM2 <thanhhm2@vng.com.vn>
2024-02-02 17:59:07 +0700

    add TextBlock

---
+++ packages/ui/TextBox/TextBoxButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import {createEventDispatcher} from 'svelte';
+
+    let className = '';
+    export {className as class};
+
+    export let element: HTMLButtonElement | null = null;
+
+    const dispatch = createEventDispatcher();
+</script>
+
+<!-- use:forwardEvents -->
+<button
+        bind:this={element}
+        class="text-box-button {className}"
+        on:click={() => dispatch('click')}
+        type="button"
+        {...$$restProps}
+>
+    <slot/>
+</button>
+
+<style lang="css">
+    @import 'TextBoxButton.css';
+</style>
