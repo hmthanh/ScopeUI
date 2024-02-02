7f4fe0c019b2384f578303cf18ba2d3e339cb561
ThanhHM2 <thanhhm2@vng.com.vn>
2024-02-02 18:00:29 +0700

    add FlyoutSurface

---
+++ packages/ui/Flyout/FlyoutSurface.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	/** Specifies a custom class name for the surface. */
+	let className = '';
+	export { className as class };
+
+	/** Obtains a bound DOM reference to the surface element. */
+	export let element: HTMLDivElement = null;
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<div class="flyout {className}" bind:this={element} {...$$restProps}>
+	<slot />
+</div>
+
+<style lang="scss">
+  @use "FlyoutSurface";
+</style>
