8c35eac7b6a745833ebb3dbd382b6e718e952513
ThanhHM2 <thanhhm2@vng.com.vn>
2024-02-02 17:58:41 +0700

    add TooltipSurface

---
+++ packages/ui/Tooltip/TooltipSurface.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	/* Specifies a custom class name for the surface. */
+	let className = '';
+	export { className as class };
+
+	/** Obtains a bound DOM reference to the surface element. */
+	export let element: HTMLDivElement = null;
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<div
+	class="tooltip {className}"
+	role="tooltip"
+	bind:this={element}
+	{...$$restProps}
+>
+	<slot />
+</div>
+
+<!--	use:forwardEvents-->
+<!-- // import { createEventForwarder } from "$lib/internal";
+	// import { get_current_component } from "svelte/internal"; -->
+
+<style lang="scss">
+  @use 'TooltipSurface';
+</style>
